3584888110c6003cd0d0c0e56738606efb2184c3
NineWoranop <nineworanop@gmail.com>
2020-10-21 11:58:10 +0700

    Remove unused code

--- docker/sonarque/start_with_profile.sh
+++ docker/sonarque/start_with_profile.sh
@@ -5,6 +5,7 @@
 
 LANGUAGE=java
 PROFILE_NAME=CustomProfile
+PROFILE_FILE=java-customprofile.xml
 BASE_URL=http://127.0.0.1:9000
 
 function isUp {
@@ -20,7 +21,9 @@ do
 done
 
 # Restore qualityprofile
-curl -v -u admin:admin -F "backup=@/qualityprofile/java-standardprofile.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
-curl -v -u admin:admin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
+curl -v -u admin:admin -F "backup=@/qualityprofile/$PROFILE_FILE" -X POST "$BASE_URL/api/qualityprofiles/restore"
+
+# Set default profile
+curl -v -u admin:admin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&qualityProfile=$PROFILE_NAME"
 
 wait
